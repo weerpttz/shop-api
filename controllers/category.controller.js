a55f5ba99edac3c361320aaf386eed596a2437bc
Weerapong Worasit <weerpttz@192.168.1.18>
2025-01-18 14:10:41 +0700

    edit

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -28,56 +28,5 @@ export const categoryControllers = {
         } catch (error) {
             res.error(error)
         }
-    },
-
-    async onCreate (req, res) { //For Admin
-        try {
-            const { name } = req.body.data
-            await Category.create({
-                name,
-                createBy: req.auth.id
-            })
-            res.send("CREATE CATEGORY")
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-    async onUpdate (req, res) { //For Admin
-        try {
-            const category = await Category.findByPk(req.params.id)
-            if (!category) {
-                return res.send("You are not authorization.")
-            }
-            if(category.createBy.toString() === req.auth.id.toString() && category.id.toString() === req.params.id.toString()) {
-                category.name = req.body.name
-                category.updateBy = req.auth.id
-            } else {
-                return res.send("You are not authorization.")
-            }
-            category.save()
-            res.send("EDIT CATEGORY")
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-    async onDelete (req, res) { //For Admin
-        try {
-            const category = await Category.findByPk(req.params.id)
-            if (!category) {
-                return res.send("You are not authorization.")
-            }
-            if(category.createBy.toString() === req.auth.id.toString() && category.id.toString() === req.params.id.toString()) {
-                category.deleteBy = req.auth.id
-                category.destroy()
-            } else {
-                return res.send("You are not authorization.")
-            }
-            category.save()
-            res.send("DELETE CATEGORY")
-        } catch (error) {
-            res.error(error)
-        }
     }
 }
